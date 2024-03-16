b0cfa3d193acca8785cc0bab78083fc587e4fe41
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-16 05:02:08 +0300

    added configurations and dependency injections , (services for Identity , Entity framework, Authantication , Jwt Bearer , and DI for token services ,, and for using these , added UseCors , and UseAuthentication)

--- JWTTokens/Program.cs
+++ JWTTokens/Program.cs
@@ -1,3 +1,12 @@
+using JWTTokens.Models.Domain;
+using JWTTokens.Repositories.Abstract;
+using JWTTokens.Repositories.Domain;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,40 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// for entity framework
+builder.Services.AddDbContext<Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("conn")));
+
+// For Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<Context>()
+    .AddDefaultTokenProviders();
+
+// For Adding Authantication
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+// For Adding Jwt Bearer 
+.AddJwtBearer(opt =>
+{
+    opt.SaveToken = true;
+    opt.RequireHttpsMetadata = false;
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
+
+// for dependency Injection
+builder.Services.AddTransient<ITokenService, TokenService>();
 
 var app = builder.Build();
 
@@ -18,6 +61,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// for Cors 
+app.UseCors(opt => opt.WithOrigins("*")
+.AllowAnyOrigin()
+.AllowAnyMethod()
+.AllowAnyHeader());
+
+// for authantication
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
