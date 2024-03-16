25418857ad68136dd57b9f37cd18e55cc47c94aa
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-16 05:09:39 +0300

    for testing , added .. clearly it works

---
+++ JWTTokens/Controllers/ProtectedController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTTokens.Controllers
+{
+    [Route("api/[controller]/{action}")]
+    [ApiController]
+    [Authorize]
+    public class ProtectedController : ControllerBase
+    {
+        public IActionResult GetData()
+        {
+            return Ok("Data from protected controller");
+        }
+    }
+}
