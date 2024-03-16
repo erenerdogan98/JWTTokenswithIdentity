3dc877ac2cd8605dff9b1e180e863599bf278201
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-16 05:02:38 +0300

    for Json Web Token , added services (used repository design pattern) ...

---
+++ JWTTokens/Repositories/Abstract/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using JWTTokens.Models.DTO;
+using System.Security.Claims;
+
+namespace JWTTokens.Repositories.Abstract
+{
+    public interface ITokenService
+    {
+        TokenResponse GetToken(IEnumerable<Claim> claim);
+        string GetRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
